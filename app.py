cbc72b14c1ffea2e92df7afbd0fffbb44e83a6af
casual3547 <adejumolae@gmail.com>
2026-07-17 04:20:53 -0700

    Prepare app for deployment

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ import os
 
 app = Flask(__name__)
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///inventory.db"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///inventory.db")
 db.init_app(app)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 
